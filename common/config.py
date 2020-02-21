cad6eeb49dbc08e19f9581b66f897afee9f4ee1a
Duy Huynh <hvd.huynhduy@gmail.com>
2020-02-21 15:17:50 +0700

    Add Mongo DB

--- common/config.py
+++ common/config.py
@@ -84,6 +84,12 @@ def get_prodenv():
     conf['elastic_password'] = os.environ.get('ELASTIC_PASSWORD', 'changeme')
     conf['elastic_index'] = os.environ.get('ELASTIC_INDEX', 'visee')
 
+    # mongodb configuration
+    conf['mongodb_host'] = os.environ.get('MONGODB_HOST', 'localhost')
+    conf['mongodb_port'] = os.environ.get('MONGODB_PORT', 27017)
+    conf['mongodb_user'] = os.environ.get('MONGODB_USER', 'visee')
+    conf['mongodb_password'] = os.environ.get('MONGODB_PASSWORD', 'visee')
+
     # other
     conf['chromedriver_path'] = os.environ.get('CHROMEDRIVER_PATH', '/visee/static/chromedriver')
     conf['image_size'] = os.environ.get('IMAGE_SIZE', 1000)
