777ea25f6c7c40e8aaead447a5fff1c861c7fd9c
hoangperry <prodai9573@gmail.com>
2020-06-30 12:45:16 +0700

    update server completer

--- web/query_completer/src/web_server.cpp
+++ web/query_completer/src/web_server.cpp
@@ -74,8 +74,7 @@ static void ev_handler(struct mg_connection* nc, int ev, void* p) {
                 data += "]}\n";
             }
             /* Send headers */
-            mg_printf(nc, "%s",
-                      "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
+            mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\nAccess-Control-Allow-Origin: *\r\n\r\n");
             mg_printf_http_chunk(nc, data.c_str(), data.size());
             mg_send_http_chunk(nc, "",
                                0);  // send empty chunk, the end of response
