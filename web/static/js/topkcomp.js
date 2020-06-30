cd297af48f5e9af56b09c0aa739b2deb7a8461ab
hoangperry <prodai9573@gmail.com>
2020-06-30 10:14:51 +0700

    ADD JS SCRIPT FOR AUTO COMPLETER

---
+++ web/static/js/topkcomp.js
@@ -0,0 +1,39 @@
+﻿/*jslint  browser: true, white: true, plusplus: true */
+
+$(function () {
+    'use strict';
+
+    // Initialize ajax autocomplete:
+    $('#autocomplete').autocomplete({
+        noCache: true,
+        serviceUrl: 'http://127.0.0.1:8080/topcomp',
+        dataType: 'jsonp',
+        minChars: '0',
+        params: {k: 10},
+        formatResult: function(suggestion, currentValue) {
+            // Do not replace anything if the current value is empty
+            if (!currentValue) {
+                return suggestion.value;
+            }
+
+            // match any tokens, not just prefix
+            var keywords = currentValue.trim().split(/\s+/).join('|');
+
+            return suggestion.value
+                .replace(new RegExp("(" + keywords + ")", "gi"), '<strong>$1<\/strong>')
+                .replace(/&/g, '&amp;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;')
+                .replace(/"/g, '&quot;')
+                .replace(/&lt;(\/?strong)&gt;/g, '<$1>');
+        },
+        paramName: 'q',
+        lookupFilter: function(suggestion, originalQuery, queryLowerCase) {
+            var re = new RegExp('\\b' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi');
+            return re.test(suggestion.value);
+        },
+        onSelect: function(suggestion) {
+            //window.location = 'http://google.com/maps?q=' + suggestion.value;
+        }
+    });
+});
